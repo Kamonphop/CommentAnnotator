bbe40d9bbd363bbf9e7634a418c14b0310bf0fb5
Abhi Karmakar <abhi.karmakar604@gmail.com>
2017-07-28 15:17:37 -0700

    added test source java files

---
+++ data-test-cc/java_src_1.java
@@ -0,0 +1,11 @@
+// my first java program
+public class java_src_1 {
+    
+}
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//     }
+/* this program prints:
+ * Hello, World!
+ * to the console */
+// }
