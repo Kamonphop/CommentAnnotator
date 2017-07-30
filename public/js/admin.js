e1fbc959fbf671c1ba737ae06ba3343d1f1353f6
Abhi Karmakar <abhi.karmakar604@gmail.com>
2017-07-30 12:09:57 -0700

    Revert "Merge branch 'master' of https://github.com/Kamonphop/CommentAnnotator into code-comments"
    This reverts commit ebffd5ff04b5e1378d41a6550b228510d93c2b9b, reversing
    changes made to 0fe58b36ed9e282cf7207dcc9479acb2c0b7d0a2.

--- public/js/admin.js
+++
@@ -1,9 +0,0 @@
-$("#expand_C0").click(function() {
-	$(".table .toggleDisplayC0").toggleClass("in");
-});
-$("#expand_C1").click(function() {
-	$(".table .toggleDisplayC1").toggleClass("in");
-});
-$("#expand_C2").click(function() {
-	$(".table .toggleDisplayC2").toggleClass("in");
-});
